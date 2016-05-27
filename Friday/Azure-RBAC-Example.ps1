26d91d2f754634654b495cfb84718f2396aa3edf
azureinterface <azureinterface@protonmail.com>
2016-05-27 07:18:42 -0700

    added rbac script

---
+++ Friday/Azure-RBAC-Example.ps1
@@ -0,0 +1,16 @@
+﻿<#
+
+https://azure.microsoft.com/en-us/documentation/articles/role-based-access-control-manage-access-powershell/
+
+#>
+
+#Creating and assigning a custom role
+
+$role = Get-AzureRmRoleDefinition 'Virtual Machine Contributor'
+$role.Actions.Add('Microsoft.Automation/automationAccounts/jobs/read')
+Set-AzureRmRoleDefinition -Role $role
+
+$user = Get-AzureRmADUser -UserPrincipalName dave@psdemoawsgmail.onmicrosoft.com
+
+$sub = Get-AzureRmSubscription -SubscriptionName 'Azure Pass'
+New-AzureRmRoleAssignment -ObjectId $user.Id -RoleDefinitionName 'Virtual Machine Contributor' -Scope '/subscriptions/a4fb11b2-a79c-4e9b-b0de-7272a7153ea3'
