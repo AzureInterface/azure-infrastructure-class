67675645a64eb381de1439e54eddac08a70e8908
AzureInterface <azureinterface@protonmail.com>
2016-08-09 09:18:34 -0700

    add example

---
+++ Monday/Lab3/example-add-srv.ps1
@@ -0,0 +1,10 @@
+﻿$cred = Get-Credential
+
+$servers = "srv1","srv2"
+
+foreach($server in $servers) {
+    .\Add-AzureRmVm.ps1 -VMName $server `
+    -ResourceGroupName TestServers `
+    -StorageAccountName 78961559 `
+    -Credential $cred
+}
