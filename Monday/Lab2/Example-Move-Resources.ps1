bf8ab95e7ac60ef2685937d4c7d470d9e8da8cf1
AzureInterface <azureinterface@protonmail.com>
2016-10-17 13:45:04 -0700

    Update Example-Move-Resources.ps1

--- Monday/Lab2/Example-Move-Resources.ps1
+++ Monday/Lab2/Example-Move-Resources.ps1
@@ -3,5 +3,9 @@ $r = Get-AzureRmResource -ResourceName mikepf16 -ResourceGroupName servers
 Move-AzureRmResource -ResourceId $r.ResourceId `
 -DestinationResourceGroupName servers2
 
+# here's another way to do it using the pipeline 
 
+Get-AzureRmResource -ResourceName mikepf16 `
+-ResourceGroupName servers2 | 
+    Move-AzureRmResource -DestinationResourceGroupName servers
 
