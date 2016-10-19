169a2cca447b118b0859ad531fc858c8ea1135a4
AzureInterface <azureinterface@protonmail.com>
2016-10-18 17:40:54 -0700

    Update New-WindowsWebServerDSC.ps1

--- Tuesday/Lab2/New-WindowsWebServerDSC.ps1
+++ Tuesday/Lab2/New-WindowsWebServerDSC.ps1
@@ -1,8 +1,22 @@
-﻿<#
+<#
 
-Here's an example of how to manually run the cmdlets to push a DSC config to an Azure VM running Windows
+Note: Don't forget to update the resource group and storage account names.
 
 #>
 
-Publish-AzureRmVMDscConfiguration -ConfigurationPath d:\WindowsWebServer.ps1 -ResourceGroupName YourRGName -StorageAccountName YourSAName
-Set-AzureRmVMDscExtension -ResourceGroupName YourRGName -VMName YourVMName -Version '2.15' -ArchiveBlobName 'WindowsWebServer.ps1.zip' -ArchiveStorageAccountName YourSAName -WmfVersion 4.0 -ConfigurationName WindowsWebServer
+
+
+#upload the DSC archive to Azure Storage
+
+Publish-AzureRmVMDscConfiguration -ConfigurationPath c:\WindowsWebServer.ps1 `
+-ResourceGroupName myservers `
+-StorageAccountName myserversdiag761
+
+#Install the DSC extention and apply the config
+
+Set-AzureRmVMDscExtension -ResourceGroupName myservers `
+-VMName WinSrv -Version '2.15' `
+-ArchiveBlobName 'WindowsWebServer.ps1.zip' `
+-ArchiveStorageAccountName myserversdiag761 `
+-WmfVersion 4.0 `
+-ConfigurationName WindowsWebServer
