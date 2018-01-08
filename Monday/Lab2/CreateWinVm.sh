a3f018208c3c35a7671f4d9c50aa380a601192d4
AzureInterface <azureinterface@protonmail.com>
2018-01-08 15:12:32 -0700

    Update CreateWinVm.sh

--- Monday/Lab2/CreateWinVm.sh
+++ Monday/Lab2/CreateWinVm.sh
@@ -34,7 +34,7 @@ az vm create \
     --image win2016datacenter \
     --admin-username azureuser \
     --admin-password $AdminPassword \
-    --size Standard_D2s_v3
+    --size Standard_F2s_v2
 
 # Open port 3389 to allow RDP traffic to host.
 az vm open-port --port 3389 --resource-group Servers --name myVM
